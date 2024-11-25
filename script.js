f5e630bbf62df52ecff94dc096d88c7adc531ec7
Ravisharma-1999 <ravi829144@gmail.com>
2024-11-25 09:07:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-describe('Shape Tests', () => {
-    it('calculates area and perimeter correctly', () => {
-        cy.visit(baseUrl + "/main.html");
+// Rectangle Class
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-        cy.window().then(win => {
-            const Rectangle = win.Rectangle;
-            const Square = win.Square;
+    // Getter for width
+    get width() {
+        return this._width;
+    }
 
-            cy.stub(win.console, "log").as("consoleLog");
+    // Getter for height
+    get height() {
+        return this._height;
+    }
 
-            const rect_w = 7;
-            const rect_h = 8;
-            const sq_s = 6;
+    // Method to calculate and return the area of the rectangle
+    getArea() {
+        return this._width * this._height;
+    }
+}
 
-            const rectangle = new Rectangle(rect_w, rect_h);
-            const square = new Square(sq_s);
+// Square Class (extends Rectangle)
+class Square extends Rectangle {
+    constructor(side) {
+        // Call the parent constructor with side for both width and height
+        super(side, side);
+    }
 
-            expect(rectangle.width).to.equal(rect_w);
-            expect(rectangle.height).to.equal(rect_h);
-            expect(rectangle.getArea()).to.equal(rect_w * rect_h);
+    // Method to calculate and return the perimeter of the square
+    getPerimeter() {
+        return 4 * this.width;  // Since all sides are equal, use width (or height)
+    }
+}
 
-            expect(square.width).to.equal(sq_s);
-            expect(square.height).to.equal(sq_s);
-            expect(square.getArea()).to.equal(sq_s * sq_s);
-            expect(square.getPerimeter()).to.equal(sq_s * 4);
-        });
-    });
-});
+// Expose the classes to the global window object for Cypress testing
+window.Rectangle = Rectangle;
+window.Square = Square;
